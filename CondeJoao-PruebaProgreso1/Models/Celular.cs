bc2503bdfab551b4d096954d72d98f810d4a7799
JoaoConde12 <132232545+JoaoConde12@users.noreply.github.com>
2024-10-30 10:57:45 -0500

    Modificado los labels de los Modelos y agregado en el nav las vistas de los modelos

--- CondeJoao-PruebaProgreso1/Models/Celular.cs
+++ CondeJoao-PruebaProgreso1/Models/Celular.cs
@@ -8,14 +8,14 @@ namespace CondeJoao_PruebaProgreso1.Models
         public int Id { get; set; } 
         [Required]                                                                               // Anotación 2
         [MaxLength(50)]                                                                          // Anotación 3
-        [Display(Name = "Modelo:")]
+        [Display(Name = "Modelo")]
         public string Modelo { get; set; }
         [Required]
         [Range(2000, 2024, ErrorMessage = "El año del celular debe estar entre 2000 y 2024")]    // Anotación 4
-        [Display(Name = "Año:")]                                                                 // Anotación 5
+        [Display(Name = "Año")]                                                                 // Anotación 5
         public int Anio { get; set; }
         [Required]
-        [Display(Name = "Modelo:")]
+        [Display(Name = "Precio")]
         public double Precio { get; set; }
     }
 }
--- CondeJoao-PruebaProgreso1/Models/Conde.cs
+++ CondeJoao-PruebaProgreso1/Models/Conde.cs
@@ -10,23 +10,23 @@ namespace CondeJoao_PruebaProgreso1.Models
 
         [Required]                                                                        // Anotación 2
         [MaxLength(100)]                                                                  // Anotación 3
-        [Display(Name = "Nombre:")]                                                       // Anotación 4
+        [Display(Name = "Nombre")]                                                       // Anotación 4
         [RegularExpression(@"^[a-zA-Z\s]+$", ErrorMessage = "Solo letras y espacios")]    // Anotación 5
         public string Nombre { get; set; }
 
         [Required]
-        [Display(Name = "Presupuesto:")]
+        [Display(Name = "Presupuesto")]
         public double Presupuesto { get; set; }
 
         [Required]
-        [Display(Name = "¿Tiene trabajo?:")]
+        [Display(Name = "¿Tiene trabajo?")]
         public bool TieneTrabajo {  get; set; }
 
         [Required]
-        [Display(Name = "Fecha de nacimiento:")]
+        [Display(Name = "Fecha de nacimiento")]
         public DateTime FechaNacimiento {  get; set; }
 
-        [Display(Name = "Celular:")]
+        [Display(Name = "Celular")]
         public Celular? Celular { get; set; }
 
         [ForeignKey(nameof(Celular))]
