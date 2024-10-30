07c311a113355316d53163c37d2a2d0712f95a95
JoaoConde12 <132232545+JoaoConde12@users.noreply.github.com>
2024-10-30 08:57:52 -0500

    Añadida la relación entre modelos, asumiendo que una persona solo puede tener un celular

--- CondeJoao-PruebaProgreso1/Models/Conde.cs
+++ CondeJoao-PruebaProgreso1/Models/Conde.cs
@@ -1,4 +1,5 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace CondeJoao_PruebaProgreso1.Models
 {
@@ -6,19 +7,29 @@ namespace CondeJoao_PruebaProgreso1.Models
     {
         [Key]                                                                             // Anotación 1
         public int Id { get; set; } 
+
         [Required]                                                                        // Anotación 2
         [MaxLength(100)]                                                                  // Anotación 3
         [Display(Name = "Nombre:")]                                                       // Anotación 4
         [RegularExpression(@"^[a-zA-Z\s]+$", ErrorMessage = "Solo letras y espacios")]    // Anotación 5
         public string Nombre { get; set; }
+
         [Required]
         [Display(Name = "Presupuesto:")]
         public double Presupuesto { get; set; }
+
         [Required]
         [Display(Name = "¿Tiene trabajo?:")]
         public bool TieneTrabajo {  get; set; }
+
         [Required]
         [Display(Name = "Fecha de nacimiento:")]
         public DateTime FechaNacimiento {  get; set; }
+
+        [Display(Name = "Celular:")]
+        public Celular? Celular { get; set; }
+
+        [ForeignKey(nameof(Celular))]
+        public int IdCelular { get; set; }
     }
 }
