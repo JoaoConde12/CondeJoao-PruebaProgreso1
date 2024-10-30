efd0b11983bc5f70b78df685fb2e6c8f30ad0fa3
JoaoConde12 <132232545+JoaoConde12@users.noreply.github.com>
2024-10-30 10:49:07 -0500

    Modificado el archivo .json para la base de datos

---
+++ CondeJoao-PruebaProgreso1/Migrations/20241030142009_Migración inicial.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CondeJoao_PruebaProgreso1.Migrations
+{
+    /// <inheritdoc />
+    public partial class Migracióninicial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Celular",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Modelo = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Anio = table.Column<int>(type: "int", nullable: false),
+                    Precio = table.Column<double>(type: "float", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Celular", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Conde",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nombre = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Presupuesto = table.Column<double>(type: "float", nullable: false),
+                    TieneTrabajo = table.Column<bool>(type: "bit", nullable: false),
+                    FechaNacimiento = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    IdCelular = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Conde", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Conde_Celular_IdCelular",
+                        column: x => x.IdCelular,
+                        principalTable: "Celular",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Conde_IdCelular",
+                table: "Conde",
+                column: "IdCelular");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Conde");
+
+            migrationBuilder.DropTable(
+                name: "Celular");
+        }
+    }
+}
---
+++ CondeJoao-PruebaProgreso1/Migrations/20241030154700_Migracion a la base de datos con docker.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CondeJoao_PruebaProgreso1.Migrations
+{
+    /// <inheritdoc />
+    public partial class Migracionalabasededatoscondocker : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
