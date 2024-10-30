5612652dc42765e7c564670e3e22c4e1f5c4036f
JoaoConde12 <132232545+JoaoConde12@users.noreply.github.com>
2024-10-30 08:22:31 -0500

    Agregar archivos de proyecto.

---
+++ CondeJoao-PruebaProgreso1/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CondeJoao_PruebaProgreso1.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
