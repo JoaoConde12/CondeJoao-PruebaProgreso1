38307f385cec04030b3e350f243b6fdfa0b656fc
JoaoConde12 <132232545+JoaoConde12@users.noreply.github.com>
2024-10-30 08:34:39 -0500

    Añadido el modelo Usuario (Mi apellido, Conde)

---
+++ CondeJoao-PruebaProgreso1/Models/Conde.cs
@@ -0,0 +1,11 @@
+﻿namespace CondeJoao_PruebaProgreso1.Models
+{
+    public class Conde
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public double Presupuesto { get; set; }
+        public bool TieneTrabajo {  get; set; }
+        public DateTime FechaNacimiento {  get; set; }
+    }
+}
