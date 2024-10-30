aaffb6be69fe2c5101d628fe424d1059762352f8
JoaoConde12 <132232545+JoaoConde12@users.noreply.github.com>
2024-10-30 08:52:55 -0500

    Añadido el modelo Celular e incluidas las anotaciones en los 2 modelos (Conde y Celular)

---
+++ CondeJoao-PruebaProgreso1/Models/Celular.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CondeJoao_PruebaProgreso1.Models
+{
+    public class Celular
+    {
+        [Key]                                                                                    // Anotación 1
+        public int Id { get; set; } 
+        [Required]                                                                               // Anotación 2
+        [MaxLength(50)]                                                                          // Anotación 3
+        [Display(Name = "Modelo:")]
+        public string Modelo { get; set; }
+        [Required]
+        [Range(2000, 2024, ErrorMessage = "El año del celular debe estar entre 2000 y 2024")]    // Anotación 4
+        [Display(Name = "Año:")]                                                                 // Anotación 5
+        public int Anio { get; set; }
+        [Required]
+        [Display(Name = "Modelo:")]
+        public double Precio { get; set; }
+    }
+}
--- CondeJoao-PruebaProgreso1/Models/Conde.cs
+++ CondeJoao-PruebaProgreso1/Models/Conde.cs
@@ -1,11 +1,24 @@
-﻿namespace CondeJoao_PruebaProgreso1.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CondeJoao_PruebaProgreso1.Models
 {
     public class Conde
     {
-        public int Id { get; set; }
+        [Key]                                                                             // Anotación 1
+        public int Id { get; set; } 
+        [Required]                                                                        // Anotación 2
+        [MaxLength(100)]                                                                  // Anotación 3
+        [Display(Name = "Nombre:")]                                                       // Anotación 4
+        [RegularExpression(@"^[a-zA-Z\s]+$", ErrorMessage = "Solo letras y espacios")]    // Anotación 5
         public string Nombre { get; set; }
+        [Required]
+        [Display(Name = "Presupuesto:")]
         public double Presupuesto { get; set; }
+        [Required]
+        [Display(Name = "¿Tiene trabajo?:")]
         public bool TieneTrabajo {  get; set; }
+        [Required]
+        [Display(Name = "Fecha de nacimiento:")]
         public DateTime FechaNacimiento {  get; set; }
     }
 }
