e6b89b3f666562ba2138f5821758abcd8f1d2b58
JoaoConde12 <132232545+JoaoConde12@users.noreply.github.com>
2024-10-30 15:30:22 -0500

    Creeria que es todo, ya agregue el boton e hice el script para que salga la alerta

--- CondeJoao-PruebaProgreso1/wwwroot/js/site.js
+++ CondeJoao-PruebaProgreso1/wwwroot/js/site.js
@@ -2,3 +2,10 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+document.addEventListener('DOMContentLoaded', () =>{
+    const boton = document.getElementById("botonAlerta");
+    if (boton) {
+        boton.addEventListener("click", () => alert("Mi nombre es Joao Conde\nY mi hobbie es jugar fútbol"));
+    }
+});
